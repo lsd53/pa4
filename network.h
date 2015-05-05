5be25894fd8b59c45a938e569b50b3b932a1c331
lsd53 <lsd53@cornell.edu>
2015-05-05 17:17:06 -0400

    Initial commit

---
+++ network.h
@@ -0,0 +1,12 @@
+//Initializes network driver use the same approach as the keyboard driver.
+void network_init();
+
+void network_recieve();
+
+void network_set_interrupts(int opt);
+
+
+void network_poll();
+
+void network_trap();
+
