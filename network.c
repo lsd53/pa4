ceae5eb845fd906a8d08a12cd025dee518722cab
lsd53 <lsd53@cornell.edu>
2015-05-05 18:19:36 -0400

    finished network_start_receive()

--- network.c
+++ network.c
@@ -21,8 +21,9 @@ void network_init(){
   }
   /* allocate room for ring buffer and set rx_base to start of the array */
   struct dma_ring_slot* ring=(struct dma_ring_slot*) malloc(sizeof(struct dma_ring_slot) * RING_SIZE);
-  dev_net[8] = virtual_to_physical((void *) ring);
-  
+  dev_net->rx_base =  virtual_to_physical((void *) ring); //ASK ABOUT THIS PART
+  dev_net->rx_capacity = RING_SIZE;  
+
   /* allocate room for each buffer in the ring and set dma_base and dma_len to appropriate values */
   for (i = 0; i < RING_SIZE; i++) {
     void* space = malloc(BUFFER_SIZE);
@@ -30,3 +31,15 @@ void network_init(){
     ring[i].dma_len = BUFFER_SIZE;
   }
 }
+
+void network_start_recieve(){
+  // write command into cmd register in order to turn on network card
+  dev_net->cmd = NET_SET_POWER;
+  dev_net->data = 1;
+ 
+  // allow network card to recieve packets 
+  dev_net->cmd = NET_SET_RECEIVE
+  dev_net->data = 1; 
+}
+
+
